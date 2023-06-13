e8af6b00220a66e214f42fd1ce94465e2cd2d674
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-13 12:06:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a){
+    
+    return (a-42)||(42-a)
+
+}
+console.log(distanceFromHqInBlocks(50))
+console.log(distanceFromHqInBlocks(34))
+
+function distanceFromHqInFeet(){
+   return distanceFromHqInBlocks(50)*264
+}
+function distanceTravelledInFeet(a,b){
+    return (b-a)*264
+
+}
+console.log(43,48)
+
+
+
